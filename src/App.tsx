77f5d0b8523377ef99b573b646b34594377a043e
bobikenobi12 <borislav1207@gmail.com>
2023-09-06 22:09:47 +0300

    add auth states

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { SignInButton } from "@clerk/clerk-react";
+import { SignInButton, useUser, SignOutButton } from "@clerk/clerk-react";
+import { AuthLoading, Authenticated, Unauthenticated } from "convex/react";
 
 function App() {
+	const { user } = useUser();
+
 	return (
 		<div className="App">
-			<SignInButton mode="modal" />
+			<AuthLoading>
+				<h1>Loading</h1>
+			</AuthLoading>
+			<Authenticated>
+				{user && <h1>Logged in as {user.id}</h1>}
+
+				<SignOutButton />
+			</Authenticated>
+			<Unauthenticated>
+				<h1>Unauthenticated</h1>
+				<SignInButton />
+			</Unauthenticated>
 		</div>
 	);
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-// interface ImportMetaEnv {
-// 	readonly REACT_APP_CLERK_PUBLISHABLE_KEY: string;
-// 	readonly VITE_CONVEX_URL: string;
-// 	// more env variables...
-// }
-
-// interface ImportMeta {
-// 	readonly env: ImportMetaEnv;
-// }
